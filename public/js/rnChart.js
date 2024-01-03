eb0a46342e2516be431971407802b796029d9c57
ITYRANNOSAURUS <po04134@naver.com>
2024-01-03 16:56:42 +0900

    0103

--- public/js/rnChart.js
+++ public/js/rnChart.js
@@ -32,24 +32,27 @@ const rnChart = new Chart(rnCtx, {
     options: {
         title: {
             display: true,
-            text: '신재생에너지 공급 비중'
+            text: '신재생에너지 공급'
         }
     }
 });
 
 window.updateRnChart = function (year) {
-    let newColor, reColor;
+    let newColor, reColor, sumColor;
 
     // 색상 결정 로직
     if (year < 2022) {
         newColor = 'red';
         reColor = 'red';
+        sumColor = 'red';
     } else if (year < 2040) {
         newColor = 'blue';
         reColor = 'blue';
+        sumColor = 'blue';
     } else {
         newColor = 'green';
         reColor = 'green';
+        sumColor = 'green';
     }
 
     // 신에너지 데이터 업데이트
@@ -67,30 +70,41 @@ window.updateRnChart = function (year) {
     } else {
         updateChartData(rnChart, year, reEnergy, reColor, 1);
     }
+
+    // 신재생에너지 데이터 업데이트
+    let sumEnergy = powerSumData[year];
+    if (sumEnergy === undefined) {
+        alert(year + "년도의 신재생에너지 데이터는 아직 준비되지 않았습니다.");
+    } else {
+        updateChartData(rnChart, year, sumEnergy, sumColor, 2);
+    }
 }
 
 // 차트 데이터 업데이트 함수
 function updateChartData(chart, year, data, color, datasetIndex) {
     let index = chart.data.labels.indexOf(year.toString());
 
-    // borderColor 배열이 없다면 초기화합니다.
-    if (!chart.data.datasets[datasetIndex].pointBorderColor) {
-        chart.data.datasets[datasetIndex].pointBorderColor = [];
+    // pointBorderColor가 배열이 아니라면, 데이터셋 크기만큼 새 배열을 생성하고 기본 색상으로 채웁니다.
+    if (!Array.isArray(chart.data.datasets[datasetIndex].pointBorderColor)) {
+        chart.data.datasets[datasetIndex].pointBorderColor = new Array(chart.data.datasets[datasetIndex].data.length).fill(chart.data.datasets[datasetIndex].borderColor);
+    }
+
+    // pointBackgroundColor가 배열이 아니라면, 데이터셋 크기만큼 새 배열을 생성하고 기본 색상으로 채웁니다.
+    if (!Array.isArray(chart.data.datasets[datasetIndex].pointBackgroundColor)) {
+        chart.data.datasets[datasetIndex].pointBackgroundColor = new Array(chart.data.datasets[datasetIndex].data.length).fill(chart.data.datasets[datasetIndex].backgroundColor);
     }
 
     if (index === -1) {
         // 새로운 연도 데이터를 추가합니다.
         chart.data.labels.push(year.toString());
         chart.data.datasets[datasetIndex].data.push(data);
-
-        // 해당 연도의 데이터 포인트에 대한 색상을 설정합니다.
         chart.data.datasets[datasetIndex].pointBorderColor.push(color);
+        chart.data.datasets[datasetIndex].pointBackgroundColor.push(color);
     } else {
         // 기존 연도의 데이터를 업데이트합니다.
         chart.data.datasets[datasetIndex].data[index] = data;
-
-        // 해당 연도의 데이터 포인트 색상만 업데이트합니다.
         chart.data.datasets[datasetIndex].pointBorderColor[index] = color;
+        chart.data.datasets[datasetIndex].pointBackgroundColor[index] = color;
     }
 
     // 차트를 업데이트합니다.
